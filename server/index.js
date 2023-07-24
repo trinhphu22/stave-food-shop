c218a59fd4e4f679fd39cbcbbbdb2b7a951f28fd
b1805859 <ngokinh30311@gmail.com>
2023-07-24 20:13:06 +0700

    deploy

--- server/index.js
+++ server/index.js
@@ -1,37 +1,35 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const authRouter = require('./routes/auth')
-
+const authRouter = require("./routes/auth");
 
 const connectDB = async () => {
-	try {
-		await mongoose.connect(
-			`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ptpmtn.xtwtw.mongodb.net/PTPMTN?retryWrites=true&w=majority`,
-			{
-				useNewUrlParser: true,
-				useUnifiedTopology: true
-			}
-		)
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ptpmtn.9cxxe.mongodb.net/`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
-		console.log('MongoDB connected')
-	} catch (error) {
-		console.log(error.message)
-		process.exit(1)
-	}
-}
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
 
-connectDB()
+connectDB();
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.use('/api/auth', authRouter)
+app.use("/api/auth", authRouter);
 
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
